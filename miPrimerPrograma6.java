0f0d64c62bbe6d2c4cb27f44bfe5d6086902cd87
Andres Nino <andresalejandroninoaraujo@gmail.com>
2025-07-04 15:54:51 -0500

    primero

---
+++ miPrimerPrograma6.java
@@ -0,0 +1,23 @@
+public class miPrimerPrograma6 { // definicion de la clase
+    public static void main(String[] args) { // metodo main
+        System.out.println("hello!"); // contenido del main
+    }
+
+    /*
+     * the first line refers to the name of the program
+     * 
+     * the second line, refers the method that execute the entry point of the program
+     * 
+     * public (access  modifier) -> it determines what outside classes can use/call the method.
+     * 
+     * static -> don't need to instance so don't need "new"
+     * 
+     * void -> type of return, void return nothing
+     * 
+     * main -> special name used for JVM (Java Virtual Machine)
+     * 
+     * String[] args -> i don't get it
+     * 
+     * 
+     */
+}
